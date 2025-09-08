a29d5c49b795efd7254b04dffc107ec2ffdb8a4f
Emith Randiv <randivemith.2003@gmail.com>
2025-09-08 21:27:42 +0530

    Add skeleton UI

--- src/components/ui/sheet.js
+++ src/components/ui/sheet.js
@@ -1,8 +1,7 @@
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
 import React from "react"
-
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
 const Sheet = SheetPrimitive.Root
 const SheetTrigger = SheetPrimitive.Trigger
